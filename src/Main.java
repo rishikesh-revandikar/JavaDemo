098548e6f03082c8f8cb9747be9eca6d7f681744
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-14 18:42:21 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+class Calculator {
+
+    public double add(double a, double b){
+        return (a+b);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("Rishikesh");
+
+        sb.append(" Revandikar");
+        System.out.println(sb);
+
+
+    }
+}
