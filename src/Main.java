f3a1f6618de250d5f79560c80c19393bd8f2bdb5
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-14 19:34:24 +0530

    Updated files

--- src/Main.java
+++ src/Main.java
@@ -5,37 +5,11 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.Random;
 
-class Calculator {
-
-    private int a;
-    private  int b;
-
-   public  Calculator(){
-       System.out.println("Hello default");
-   }
-
-   public  Calculator(int a, int b){
-       this.a = a;
-       this.b  = b;
-       System.out.println("Hello parameterized");
-   }
-
-}
-
-class AdvancedCalc extends Calculator {
-
-    public AdvancedCalc(){
-        super(1,2);
-        System.out.println("In advanced calc");
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
 
-        AdvancedCalc a = new AdvancedCalc();
 
-        Tools t = new Tools();
 
 
     }
