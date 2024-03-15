b84fb77c36b531bf8a84aade5ab0deae039def03
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-15 21:07:04 +0530

    Exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
-import NewCodes.Status;
+
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Status []s = Status.values();
-
-        for(Status x:s){
-            System.out.println(x.ordinal());
+        try{
+            int i = 2;
+            int j = 20;
+            int x = i/j;
+            System.out.println(x);
+            if(x==0)
+                throw new ArithmeticException();
+        }catch (ArithmeticException e){
+            System.out.println("Making default o/p");
         }
 
+        System.out.println("Bye");
 
     }
 }
